5ca62e4565c668ba10196feb156f31e6f7e37597
Morgan Senechal <morgan.senechal@hitachivantara.com>
2018-01-10 18:09:39 +0000

    Adding configurations
    Adding configurations file

---
+++ configuration/config-pdi-local/spoon.bat
@@ -0,0 +1,11 @@
+set PENTAHO_HOME=C:\Pentaho_8
+set PENTAHO_JAVA_HOME=C:\Pentaho_8\java
+
+set KETTLE_CLIENT_DIR="%PENTAHO_HOME%\design-tools\data-integration"
+set KETTLE_HOME=%CD%
+set KETTLE_META_HOME=%CD%
+set PENTAHO_DI_JAVA_OPTIONS="-Xms1024m" "-Xmx2048m"
+
+set OPT=%OPT% "-DPENTAHO_METASTORE_FOLDER=%KETTLE_META_HOME%"
+
+call %KETTLE_CLIENT_DIR%\Spoon.bat %OPT% %*
